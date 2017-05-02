d41a431752e451a6511b18d1349c2166cc473889
irteza_syed <irteza.syed@macaulay.cuny.edu>
2017-05-02 19:14:32 -0400

    Created Xamarin.Forms Project

---
+++ PureChat/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace PureChat
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new PureChatPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
